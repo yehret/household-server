455788648db26198043395b34709a6443a7e6cfe
Serhii <sirozhka.v@gmail.com>
2024-03-25 10:54:37 +0100

    sign up route

---
+++ controllers/auth.js
@@ -0,0 +1,23 @@
+import User from '../models/User.js'
+import bcrypt from 'bcryptjs'
+import { createError } from '../createError.js'
+import jwt from 'jsonwebtoken'
+
+export const signup = async (req, res, next) => {
+   const verifyEmail = await User.findOne({ email: req.body.email})
+   try {
+      if(verifyEmail) {
+         return createError(403, "Користувач з даною електронною поштою вже існує")
+      } else {
+         const salt = bcrypt.genSaltSync(10)
+         const hash = bcrypt.hashSync(req.body.password, salt)
+         const newUser = new User({...req.body, password: hash})
+   
+         await newUser.save()
+         res.status(200).send("Користувач успішно створений")
+      }
+   }
+   catch (error) {
+      next(error)
+   }
+}
