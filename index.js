43d6e16e9912c2e907abcb478d3ae75482251a8b
Serhii <sirozhka.v@gmail.com>
2024-06-06 21:46:22 +0200

    change cors origin

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const connect = () => {
 }
 
 app.use(cors({
-   origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
+   origin: '*',
    methods: 'GET, POST, PUT, DELETE, PATCH',
    credentials: true,
    maxAge: 3600,
