b01c7de898fbb23c0fed35324b467c59c9168bd9
Serhii <sirozhka.v@gmail.com>
2024-06-15 12:54:10 +0200

    add localhost origin

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const connect = () => {
 }
 
 app.use(cors({
-   origin: 'https://household-shop.vercel.app',
+   origin: ['https://household-shop.vercel.app', 'http://localhost:5173'],
    methods: 'GET, POST, PUT, DELETE, PATCH',
    credentials: true,
    maxAge: 3600,
